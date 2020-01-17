94f0832e02fb34c7181dd3601d2fb3a81ae274c1
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2020-01-16 22:17:37 -0300

    aula 3 - backend cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const routes = require('./routes');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const cors = require("cors");
 
 const app = express();
 
-mongoose.connect("mongodb+srv://kibutzzz:omnistack@cluster0-lxpjc.mongodb.net/week10?retryWrites=true&w=majority", {
+mongoose.connect(
+  "mongodb+srv://kibutzzz:omnistack@cluster0-lxpjc.mongodb.net/week10?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
+  }
+);
 
+app.use(cors({}));
 app.use(express.json());
 
 app.use(routes);
-
 
 app.listen(3333);
