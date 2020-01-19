a205b20501d6fe962c7db3d97b2784cd3a98eb05
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2020-01-19 18:24:08 -0300

    corrige query de busca

--- backend/src/controllers/SearchController.js
+++ backend/src/controllers/SearchController.js
@@ -2,29 +2,27 @@ const Dev = require("../models/Dev");
 const parseStringAsArray = require("../utils/parseStringAsArray");
 
 module.exports = {
-    async index(request, response) {
-        //buscar devs em 10km
-        const { latitude, longitude, techs } = request.query;
+  async index(request, response) {
+    //buscar devs em 10km
+    const { latitude, longitude, techs } = request.query;
 
-        const techsArray = parseStringAsArray(techs);
+    const techsArray = parseStringAsArray(techs);
+    const devs = await Dev.find({
+      $or: [
+        { techs: { $in: techsArray } },
+        { $expr: { $eq: [0, techsArray.length] } }
+      ],
+      location: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [longitude, latitude]
+          },
+          $maxDistance: 10000
+        }
+      }
+    });
 
-        console.log(latitude, longitude, techs);
-
-        const devs = await Dev.find({
-            techs: {
-                $in: techsArray
-            },
-            location: {
-                $near: {
-                    $geometry: {
-                        type: "Point",
-                        coordinates: [longitude, latitude]
-                    },
-                    $maxDistance: 10000
-                }
-            }
-        });
-
-        return response.json({ devs })
-    }
-}
+    return response.json({ devs });
+  }
+};
